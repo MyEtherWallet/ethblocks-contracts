65f78a9b6301bd184b7a6687281e3ce1ab01aba2
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2021-10-01 16:16:10 -0700

    devop: eth blocks contract

---
+++ contracts/EthBlocks.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./ERC721Tradable.sol";
+import "./Signature.sol";
+
+/**
+ * @title Eth Blocks
+ * ETHB - a contract for creating Ethereum block NFTs
+ */
+contract Creature is ERC721Tradable, VerifySignature {
+    address signer;
+    mapping(uint256 => uint256) public blockHashes;
+
+    constructor(address _proxyRegistryAddress, address _signer)
+        ERC721Tradable("Eth Blocks", "ETHB", _proxyRegistryAddress)
+    {
+        signer = _signer;
+    }
+
+    function contractURI() public pure returns (string memory) {
+        return "https://ethblocks.mewapi.io/contract/meta";
+    }
+
+    function changeSigner(address _newSigner) public onlyOwner {
+        signer = _newSigner;
+    }
+
+    /**
+     * @dev Mints a token to an address with a tokenURI.
+     * @param _to address of the future owner of the token
+     * @param _blockNumber block number of the block
+     * @param _tokenId uint256 of the blockHash
+     * @param _ipfsHash ipfsHash of the token URI
+     * @param _signature signature of keccak256(abi.encodePacked(_blockNumber, _tokenId, _ipfsHash)) signed by the signer
+     */
+    function mint(
+        address _to,
+        uint256 _blockNumber,
+        uint256 _tokenId,
+        string memory _ipfsHash,
+        bytes memory _signature
+    ) public onlyOwner {
+        require(
+            verifySig(_blockNumber, _tokenId, _ipfsHash, signer, _signature),
+            "EthBlocks: Not a valid signature"
+        );
+        _safeMint(_to, _tokenId);
+        _setTokenURI(_tokenId, _ipfsHash);
+        blockHashes[_blockNumber] = _tokenId;
+    }
+
+    function hashToTokenId(bytes32 _hash) public pure returns (uint256) {
+        return uint256(_hash);
+    }
+
+    function tokenIdToHash(uint256 _tokenId) public pure returns (bytes32) {
+        return bytes32(_tokenId);
+    }
+}
---
+++ contracts/Signature.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+/* Signature Verification
+
+How to Sign and Verify
+# Signing
+1. Create message to sign
+2. Hash the message
+3. Sign the hash (off chain, keep your private key secret)
+
+# Verify
+1. Recreate hash from the original message
+2. Recover signer from signature and hash
+3. Compare recovered signer to claimed signer
+*/
+
+contract VerifySignature {
+    function getMessageHash(
+        uint256 _blockNumber,
+        uint256 _tokenId,
+        string memory _ipfsHash
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(_blockNumber, _tokenId, _ipfsHash));
+    }
+
+    function getEthSignedMessageHash(bytes32 _messageHash)
+        public
+        pure
+        returns (bytes32)
+    {
+        /*
+        Signature is produced by signing a keccak256 hash with the following format:
+        "\x19Ethereum Signed Message\n" + len(msg) + msg
+        */
+        return
+            keccak256(
+                abi.encodePacked(
+                    "\x19Ethereum Signed Message:\n32",
+                    _messageHash
+                )
+            );
+    }
+
+    function verifySig(
+        uint256 _blockNumber,
+        uint256 _tokenId,
+        string memory _ipfsHash,
+        address _signer,
+        bytes memory signature
+    ) public pure returns (bool) {
+        bytes32 messageHash = getMessageHash(_blockNumber, _tokenId, _ipfsHash);
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+
+        return recoverSigner(ethSignedMessageHash, signature) == _signer;
+    }
+
+    function recoverSigner(
+        bytes32 _ethSignedMessageHash,
+        bytes memory _signature
+    ) public pure returns (address) {
+        (bytes32 r, bytes32 s, uint8 v) = splitSignature(_signature);
+
+        return ecrecover(_ethSignedMessageHash, v, r, s);
+    }
+
+    function splitSignature(bytes memory sig)
+        public
+        pure
+        returns (
+            bytes32 r,
+            bytes32 s,
+            uint8 v
+        )
+    {
+        require(sig.length == 65, "invalid signature length");
+
+        assembly {
+            /*
+            First 32 bytes stores the length of the signature
+
+            add(sig, 32) = pointer of sig + 32
+            effectively, skips first 32 bytes of signature
+
+            mload(p) loads next 32 bytes starting at the memory address p into memory
+            */
+
+            // first 32 bytes, after the length prefix
+            r := mload(add(sig, 32))
+            // second 32 bytes
+            s := mload(add(sig, 64))
+            // final byte (first byte of the next 32 bytes)
+            v := byte(0, mload(add(sig, 96)))
+        }
+
+        // implicitly return (r, s, v)
+    }
+}
